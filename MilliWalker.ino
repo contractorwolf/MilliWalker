d96d3839cbb50810c4e44085d1760117284ab580
contractorwolf <contractorwolf@hotmail.com>
2015-05-16 17:01:16 -0400

    added turning via joystick

--- MilliWalker.ino
+++ MilliWalker.ino
@@ -40,12 +40,21 @@ int pot2pin = A1;  // analog pin used to connect the potentiometer
 
 
 int speed;
-int val2;
+int bend;
+int bend_angle;
+
+int middle_joint = 1300;
+
+
 int time_delay = 300;
-int centers[] = {1366,1329,1367,1369,1407,1399,1402,1297};//1572,1132
+int centers[] = {1366,1329,1367,1369,1407,1399,1402,1297,1141};//1572,1132
 
 int vertical_sway = 250;
 int horizontal_sway = 175;
+int bend_sway = 300;
+
+
+
 int horizontal_angle;
 int vertical_angle;
 int reverse_horizontal_angle;
@@ -70,24 +79,16 @@ void setup()
 	pinMode(pot1pin, INPUT);  // backbone servos controller
 	pinMode(pot2pin, INPUT);  // leg servo controller
 
-
 	pinMode(upJoystickPin, INPUT);
 	pinMode(buttonJoystickPin, INPUT); 
 	pinMode(downJoystickPin, INPUT); 
 	pinMode(leftJoystickPin, INPUT);
 	pinMode(rightJoystickPin, INPUT);
 
-
-
-
-
-
 	Serial.begin(115200);// COM7 USB
-
 	Serial1.begin(9600);// USC32 Servo Control Board
   
-
-	Serial.println("MilliWalker v3 Started");
+	Serial.println("MilliWalker v4 Started");
 
 	delay(3000);
 }
@@ -95,19 +96,15 @@ void setup()
 
 void loop()
 {
-
-
-	speed = analogRead(pot1pin);            // reads the value of the potentiometer (value between 0 and 1023) 
-	val2 = analogRead(pot2pin);            // reads the value of the potentiometer (value between 0 and 1023) 
-	//0-1017 pot values
-	//530-2340 max values
-
+	//get joystick values
 	upPressed = digitalRead(upJoystickPin);
 	downPressed = digitalRead(downJoystickPin);  
 	leftPressed = digitalRead(leftJoystickPin);  
 	rightPressed = digitalRead(rightJoystickPin);  
 	buttonPressed = digitalRead(buttonJoystickPin);  
 
+
+	//output joystick values to serial monitor
 	Serial.print("up: ");
 	Serial.print(upPressed);
 	Serial.print("  down: ");
@@ -117,107 +114,89 @@ void loop()
 	Serial.print("  right: ");
 	Serial.print(rightPressed);  
 	Serial.print("  button: ");
-	Serial.println(buttonPressed);  
+	Serial.print(buttonPressed);  
 
 
-
-
-
+	//speed = analogRead(pot1pin);            // reads the value of the potentiometer (value between 0 and 1023) 
+	//bend = analogRead(pot2pin);            // reads the value of the potentiometer (value between 0 and 1023) 
+	//0-1017 pot values
+	//530-2340 max values
 	// show analog values
-	Serial.print("speed value: ");  
-	Serial.print(speed); 
+	//Serial.print("	speed value: ");  
+	//Serial.print(speed); 
+	//Serial.print("  bend value: ");  
+	//Serial.println(bend); 
+	//SendSingleServoCommand(9, bend, 300);
+	//bend_angle = map(bend, 0, 1023, 600, 2000); //horizontal_angle
 
-	Serial.print("analog2 value: ");  
-	Serial.println(val2); 
 
+	//change turn angle, defaults to straight
+	if(rightPressed){
+		bend_angle = centers[8] + bend_sway;
+	}else if(leftPressed){
+		bend_angle = centers[8] - bend_sway;
+	}else{
+		bend_angle = centers[8];
+	}
 
+	//change speed based on button being pressed
+	if(buttonPressed){
+		speed = 250;	
+	}else{
+		speed = 350;	
+	}
 
+	//get commands from serial monitor
 	while(Serial.available()) {
-
 		incomingCommand = "";
 		character = Serial.read();
 		incomingCommand.concat(character);
 	}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 	if (incomingCommand == "w"||upPressed) {
-		//send 4 step movements
-		//forward walking, 4 steps                    leg       leg
-		SendServoCommandList(1541,1579,1192,1119,1582,1149,1227,1547,speed);//step 1
-		SendServoCommandList(1541,1079,1192,1619,1582,1649,1227,1047,speed);//step 2
-		SendServoCommandList(1191,1079,1542,1619,1232,1649,1577,1047,speed);//step 3
-		SendServoCommandList(1191,1579,1542,1119,1232,1149,1577,1547,speed);//step 4
-
+		//forward all steps
+		SendServoCommandList(1191,1579,1542,1119,1232,1149,1577,1547,bend_angle,speed);//step 4
+		SendServoCommandList(1191,1079,1542,1619,1232,1649,1577,1047,bend_angle,speed);//step 3
+		SendServoCommandList(1541,1079,1192,1619,1582,1649,1227,1047,bend_angle,speed);//step 2
+		SendServoCommandList(1541,1579,1192,1119,1582,1149,1227,1547,bend_angle,speed);//step 1
 	}else if (incomingCommand == "r"||downPressed) {
-		//reverse all steps, walks in opposite direction
-		SendServoCommandList(1191,1579,1542,1119,1232,1149,1577,1547,speed);//step 4
-		SendServoCommandList(1191,1079,1542,1619,1232,1649,1577,1047,speed);//step 3
-		SendServoCommandList(1541,1079,1192,1619,1582,1649,1227,1047,speed);//step 2
-		SendServoCommandList(1541,1579,1192,1119,1582,1149,1227,1547,speed);//step 1
-
+		//send 4 step movements, walks in opposite direction
+		//reverse walking, 4 steps                    leg       leg
+		SendServoCommandList(1541,1579,1192,1119,1582,1149,1227,1547,bend_angle,speed);//step 1
+		SendServoCommandList(1541,1079,1192,1619,1582,1649,1227,1047,bend_angle,speed);//step 2
+		SendServoCommandList(1191,1079,1542,1619,1232,1649,1577,1047,bend_angle,speed);//step 3
+		SendServoCommandList(1191,1579,1542,1119,1232,1149,1577,1547,bend_angle,speed);//step 4
 	}else if (incomingCommand == "c"||(!downPressed && !downPressed)) {
 		//center all joints
 		//center all servos
-		SendServoCommandList(centers[0],centers[1],centers[2],centers[3],centers[4],centers[5],centers[6],centers[7],speed);
+		SendServoCommandList(centers[0],centers[1],centers[2],centers[3],centers[4],centers[5],centers[6],centers[7],bend_angle,speed);
 	}else if (incomingCommand == "1") {
 		//first step, all servos
-		SendServoCommandList(1541,1579,1192,1119,1582,1149,1227,1547,speed);//step 1
+		SendServoCommandList(1541,1579,1192,1119,1582,1149,1227,1547,bend_angle,speed);//step 1
 
 	}else if (incomingCommand == "2") {
 		//second step, all servos
-		SendServoCommandList(1541,1079,1192,1619,1582,1649,1227,1147,speed);//step 2
+		SendServoCommandList(1541,1079,1192,1619,1582,1649,1227,1147,bend_angle,speed);//step 2
 
 	}else if (incomingCommand == "3") {
 		//third step, all servos
-		SendServoCommandList(1191,1079,1542,1619,1232,1649,1577,1147,speed);//step 3
+		SendServoCommandList(1191,1079,1542,1619,1232,1649,1577,1147,bend_angle,speed);//step 3
 	}else if (incomingCommand == "4") {
 		//fourth step, all servos
-		SendServoCommandList(1191,1579,1542,1119,1232,1149,1577,1547,speed);//step 4
+		SendServoCommandList(1191,1579,1542,1119,1232,1149,1577,1547,bend_angle,speed);//step 4
 	}else if (incomingCommand != "") {
 		//defaults to center
 		//center all servos
-		SendServoCommandList(centers[0],centers[1],centers[2],centers[3],centers[4],centers[5],centers[6],centers[7],speed);
+		SendServoCommandList(centers[0],centers[1],centers[2],centers[3],centers[4],centers[5],centers[6],centers[7],bend_angle,speed);
 	}
 
-
-	////forward walking, 4 steps
-	//SendServoCommandList(1541,1579,1192,1119,1582,1649,1227,1047,val);//step 1
-	//SendServoCommandList(1541,1079,1192,1619,1582,1149,1227,1547,val);//step 2
-	//SendServoCommandList(1191,1079,1542,1619,1232,1149,1577,1547,val);//step 3
-	//SendServoCommandList(1191,1579,1542,1119,1232,1649,1577,1047,val);//step 4
-
-
-
-	////center all servos
-	//SendServoCommandList(centers[0],centers[1],centers[2],centers[3],centers[4],centers[5],centers[6],centers[7],val);
-
-
-
-
-
-	/*
-
-	SendServoCommand(val, val2, time_delay);
-
-	*/
 
 	//delay(100);
 }
 
 
-void SendServoCommandList(int servo0,int servo1,int servo2,int servo3,int servo4,int servo5,int servo6,int servo7,int time_delay){
+void SendServoCommandList(int servo0,int servo1,int servo2,int servo3,int servo4,int servo5,int servo6,int servo7,int servo8,int time_delay){
 
 	//send to USC32 servo
 	Serial1.print("#1P");
@@ -236,6 +215,9 @@ void SendServoCommandList(int servo0,int servo1,int servo2,int servo3,int servo4
 	Serial1.print(servo6);
 	Serial1.print("#8P");
 	Serial1.print(servo7);
+
+	Serial1.print("#9P");
+	Serial1.print(servo8);
 	Serial1.print("T");
 	Serial1.println(time_delay);
 
@@ -256,6 +238,11 @@ void SendServoCommandList(int servo0,int servo1,int servo2,int servo3,int servo4
 	Serial.print(servo6);
 	Serial.print("#8P");
 	Serial.print(servo7);
+
+	Serial.print("#9P");
+	Serial.print(servo8);
+
+
 	Serial.print("T");
 	Serial.println(time_delay);
 
@@ -263,15 +250,7 @@ void SendServoCommandList(int servo0,int servo1,int servo2,int servo3,int servo4
 }
 
 
-
-
-
-
-
-
-
-
-
+//for walk testing only
 void SendServoCommand(int pot_angle, int pot_angle2, int time_delay){
 
 	//first segment responding to potentiometers
@@ -333,188 +312,27 @@ void SendServoCommand(int pot_angle, int pot_angle2, int time_delay){
 }
 
 
+//for testing sending a single value
+void SendSingleServoCommand(int servo, int pot_angle, int time_delay){
 
+	//first segment responding to potentiometers
+	int servo_angle = map(pot_angle, 0, 1023, 600, 2000); //horizontal_angle
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-#define CENTER1 1400
-#define HIGH1 1100
-#define LOW1 1500
-#define CENTER2 1400
-#define HIGH2 1600
-#define LOW2 1200
-#define CENTER3 1400
-#define HIGH3 1500
-#define LOW3 1100
-#define CENTER4 1400
-#define HIGH4 1600
-#define LOW4 1200
-#define CENTER5 1400
-#define HIGH5 1500
-#define LOW5 1200
-#define CENTER6 1400
-#define HIGH6 1600
-#define LOW6 1250
-#define CENTER7 1400
-#define HIGH7 1500
-#define LOW7 1100
-#define CENTER8 1400
-#define HIGH8 1600
-#define LOW8 1200
-
-
-
-
-
-
-int led = 13;
-String incomingCommand = "c";
-char character;
-
-
-
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);  
-
-  Serial.begin(115200);
-  Serial1.begin(9600);
-  
-
-  Serial.println("MilliWalker v1.0 Started");
-
-  delay(3000);
-
-
-
-}
-
-void loop()
-{
-
-	
-	int time_delay = 300;
-
-	while(Serial.available()) {
-
-		incomingCommand = "";
-		character = Serial.read();
-		incomingCommand.concat(character);
-	}
-
-	if (incomingCommand == "w") {
-		//send 4 step movements
-		SendFirstCommand(time_delay);
-		SendSecondCommand(time_delay);
-		SendThirdCommand(time_delay);
-		SendFourthCommand(time_delay);
-	}else if (incomingCommand == "r") {
-		//reverse all steps, walks in opposite direction
-		SendFourthCommand(time_delay);
-		SendThirdCommand(time_delay);
-		SendSecondCommand(time_delay);		
-		SendFirstCommand(time_delay);
-	}else if (incomingCommand == "c") {
-		//center all joints
-		SendCenterCommand(time_delay);
-	}else if (incomingCommand == "1") {
-		//first step, all servos
-		SendFirstCommand(time_delay);
-
-	}else if (incomingCommand == "2") {
-		//second step, all servos
-		SendSecondCommand(time_delay);
-	}else if (incomingCommand == "3") {
-		//third step, all servos
-		SendThirdCommand(time_delay);
-	}else if (incomingCommand == "4") {
-		//fourth step, all servos
-		SendFourthCommand(time_delay);
-	}else if (incomingCommand != "") {
-		//defaults to center
-		SendCenterCommand(time_delay);
-	}
-
-
-
-
-
-
-
-
-
-
-
-		//flash led
-    digitalWrite(led, HIGH);   
-    delay(30);               
-    digitalWrite(led, LOW); 
-	//delay(100);  
-}
-
-
-void SendCenterCommand(int time_delay){
-	SendServoCommand("#1P1300#2P1380#3P1400#4P1380#5P1400#6P1425#7P1320#8P1340",time_delay);
-}
-
-
-
-
-//4 steps***********************
-void SendFirstCommand(int time_delay){
-	SendServoCommand("#1P1100#2P1200#3P1500#4P1560#5P1500#6P1220#7P1100#8P1530",time_delay);
-}
-void SendSecondCommand(int time_delay){
-	SendServoCommand("#1P1500#2P1200#3P1100#4P1560#5P1200#6P1220#7P1500#8P1530",time_delay);
-}
-void SendThirdCommand(int time_delay){
-	SendServoCommand("#1P1500#2P1530#3P1100#4P1200#5P1200#6P1590#7P1500#8P1190",time_delay);
-}
-void SendFourthCommand(int time_delay){
-	SendServoCommand("#1P1100#2P1530#3P1500#4P1200#5P1500#6P1590#7P1100#8P1190",time_delay);
-}
-
-
-
-
-
-void SendServoCommand(String command, int time_delay){
-	Serial1.print(command);
+	//send to USC32 servo
+	Serial1.print("#");
+	Serial1.print(servo);
+	Serial1.print("P");
+	Serial1.print(servo_angle);
 	Serial1.print("T");
 	Serial1.println(time_delay);
-	//Serial.print(command);
-	//Serial.print("T");
-	//Serial.println(time_delay);
-	delay(time_delay); 
-	//Serial.println(command);
-}
 
-void SendServoStatement(int chan,int ang, int time_delay){
-    Serial.print(String("#"));
-    Serial.print(String(chan));
-    Serial.print(String("P"));
-    Serial.print(String(ang));
-    Serial.print(String("T"));
-    Serial.println(String(time_delay));
+	
+	//send to USC32 servo
+	Serial.print("#");
+	Serial.print(servo);
+	Serial.print("P");
+	Serial.print(servo_angle);
+	Serial.print("T");
+	Serial.println(time_delay);
 
-    Serial1.print(String("#"));
-    Serial1.print(String(chan));
-    Serial1.print(String("P"));
-    Serial1.print(String(ang));
-    Serial1.print(String("T"));
-    Serial1.println(String(time_delay));
 }
-*/
